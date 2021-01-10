5074dd40ec9a1b4d6a2e2e48bb0383b150c7e700
Ian Gerolamo <iangerolamo@gmail.com>
2021-01-10 19:07:39 -0300

    POST /orders

--- backend/src/main/java/com/igerolamo/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/igerolamo/dsdeliver/services/OrderService.java
@@ -2,12 +2,17 @@ package com.igerolamo.dsdeliver.services;
 
 
 import com.igerolamo.dsdeliver.dto.OrderDTO;
+import com.igerolamo.dsdeliver.dto.ProductDTO;
 import com.igerolamo.dsdeliver.entities.Order;
+import com.igerolamo.dsdeliver.entities.OrderStatus;
+import com.igerolamo.dsdeliver.entities.Product;
 import com.igerolamo.dsdeliver.repositories.OrderRepository;
+import com.igerolamo.dsdeliver.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,11 +23,25 @@ public class OrderService {
     @Autowired
     private OrderRepository repository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Transactional(readOnly = true)
     public List<OrderDTO> findAll() {
         List<Order> list = repository.findOrdersWithProducts();
         return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
     }
 
+    @Transactional
+    public OrderDTO insert(OrderDTO dto) {
+        Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+                Instant.now(), OrderStatus.PENDING);
+        for (ProductDTO p : dto.getProducts()) {
+            Product product = productRepository.getOne(p.getId());
+            order.getProducts().add(product);
+        }
+        order = repository.save(order);
+        return new OrderDTO(order);
+    }
 }
 

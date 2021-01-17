c9412ee55edf2549abf3a5404df26d7cffbcea2d
Ian Gerolamo <iangerolamo@gmail.com>
2021-01-17 09:24:49 -0300

    Configurando os estilos globais

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
-  <h1>DSdeliver</h1>
+  <>
+    <h1>DSdeliver</h1>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
---
+++ front-web/src/styles/global.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+  body {
+    background: #E5E5E5;
+    -webkit-font-smoothing: antialiased;
+  }
+  body, input, button {
+    font-family: 'Open Sans', sans-serif;
+  }
+  button {
+    cursor: pointer;
+  }
+`;
